0c1e99bf35aea964ded1e9ad5c499d3e1af8e55f
Ameen shariff <ameenshariff0319@gmail.com>
2023-12-04 21:13:25 +0530

    few changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let string="";
 let buttons= document.querySelectorAll('.button')
-Array.from(buttons).forEach((buttons)=>{
-    buttons.addEventListener('click',(e)=>{
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
         if(e.target.innerHTML == '='){
             string = eval(string);
             document.querySelector('input').value=string;
